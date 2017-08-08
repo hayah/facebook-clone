8b82c629b37e5ad55421f8f953df745de030dac2
Alexander A~ <suvarin@yandex.ru>
2017-08-08 14:46:04 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  root 'users#show'
+  resources :users
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
