c3dd9ab463c7dc03fdc97f2147971dff88d0a8f3
Alexander A~ <suvarin@yandex.ru>
2017-08-08 15:22:56 +0300

    Add posts#create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def create
+    @user = User.first
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to request.referer
+    else
+      render root_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
