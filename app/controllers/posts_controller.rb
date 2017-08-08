b2e188e301f38d399a95ccbeea4a8a02e63ae5c0
Alexander A~ <suvarin@yandex.ru>
2017-08-08 15:55:17 +0300

    Add posts#edit and #update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to user_path(@post.user)
+    else
+      render :edit
+    end
+  end
+
   def create
     @user = User.first
     @post = @user.posts.build(post_params)
