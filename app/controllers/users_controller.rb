6614ef33df75a516af15f1a5f14e156457df5d6a
Alexander A~ <suvarin@yandex.ru>
2017-08-08 20:46:22 +0300

    Add users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @post = Post.new
     @posts = Post.where(user_id: params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_url
+    else
+      render root_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
