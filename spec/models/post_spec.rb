1e79722b0c2e53d660212599d80b5dace27001a8
Alexander A~ <suvarin@yandex.ru>
2017-08-08 16:20:27 +0300

    Add validations to post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :user }
+  it { is_expected.to validate_presence_of :content }
+  it { is_expected.to validate_length_of(:content).is_at_most(2000) }
 end
