e5f22fef43b5692964dbf229082b5ebc6ef97e8a
Alexander A~ <suvarin@yandex.ru>
2017-08-08 22:01:35 +0300

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :posts
+
+  validates :first_name, :last_name, presence: true, length: { in: 2..30 }
+  validates :password, length: { in: 6..64 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_length_of(:first_name).
+       is_at_least(2).is_at_most(30) }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_length_of(:last_name).
+       is_at_least(2).is_at_most(30) }
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_length_of(:password).
+       is_at_least(6).is_at_most(64) }
+  it { is_expected.to validate_presence_of :email }
 end
