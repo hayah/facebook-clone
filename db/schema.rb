c505829a75a838e35adced250d4e54c928a8d37b
Alexander A~ <suvarin@yandex.ru>
2017-08-08 14:42:13 +0300

    Generate User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808113321) do
+ActiveRecord::Schema.define(version: 20170808114133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "phone_number"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    username "MyString"
+    phone_number "MyString"
+    email "MyString"
+  end
+end
