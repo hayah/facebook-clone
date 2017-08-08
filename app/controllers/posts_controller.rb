80856b1cf0af948b22654559320782d8c90bb385
Alexander A~ <suvarin@yandex.ru>
2017-08-08 15:50:20 +0300

    Add posts#destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to request.referer
+  end
+
   private
 
   def post_params
