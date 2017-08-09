8c206370f975698275c7178e00b76198e18fdf0f
Alexander A~ <suvarin@yandex.ru>
2017-08-09 18:48:18 +0300

    Add full_name method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, length: { in: 2..30 }
   validates :password, length: { in: 6..64 }
   validates :email, uniqueness: { case_sensitive: false }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
